1da2bcc99bf40bd875156b06be1a56ffcce734ec
monster <monster@hamsterbase.com>
2024-02-08 01:31:08 +0800

    fix: change Postings

--- src/core/postings.ts
+++ src/core/postings.ts
@@ -11,58 +11,52 @@ export class Postings implements IPostings {
   public amount: IAmount;
   public metadata?: Record<string, string>;
   public as?: IPostingsAs;
-  constructor(option: IPostings, as?: IPostingsAs) {
+  constructor(option: IPostings) {
     this.account = option.account;
     this.amount = option.amount;
     this.metadata = option.metadata;
-    this.as = as;
+    this.as = option.as;
   }
 
   asPrice(value: number, currency: ICurrency): IPostings {
-    return new Postings(
-      {
-        account: this.account,
-        amount: this.amount,
-        metadata: this.metadata,
-      },
-      {
+    return new Postings({
+      account: this.account,
+      amount: this.amount,
+      metadata: this.metadata,
+      as: {
         type: "price",
         amount: {
           value,
           currency,
         },
-      }
-    );
+      },
+    });
   }
 
   asCost(value: number, currency: ICurrency): IPostings {
-    return new Postings(
-      {
-        account: this.account,
-        amount: this.amount,
-        metadata: this.metadata,
-      },
-      {
+    return new Postings({
+      account: this.account,
+      amount: this.amount,
+      metadata: this.metadata,
+      as: {
         type: "cost",
         amount: {
           value,
           currency,
         },
-      }
-    );
+      },
+    });
   }
 
   meta(key: string, value: string) {
-    return new Postings(
-      {
-        account: this.account,
-        amount: this.amount,
-        metadata: {
-          ...this.metadata,
-          [key]: value,
-        },
+    return new Postings({
+      account: this.account,
+      amount: this.amount,
+      metadata: {
+        ...this.metadata,
+        [key]: value,
       },
-      this.as
-    );
+      as: this.as,
+    });
   }
 }
