7ac21040bb619368ddd5a30c983faff55640c17b
monster <monster@hamsterbase.com>
2024-03-15 22:25:42 +0800

    feat: support pad

--- src/utils/pad-account.ts
+++ src/utils/pad-account.ts
@@ -5,7 +5,37 @@ export function padAccount(account: Account) {
   return (old: ITransaction): ITransaction => {
     let init = new Decimal(0);
     old.postings.forEach((posting) => {
-      init = init.add(posting.amount.value);
+      if (posting.as) {
+        if (posting.as.type === "cost") {
+          if (posting.amount.value < 0) {
+            init = init.minus(posting.as.amount.value);
+          } else {
+            init = init.add(posting.as.amount.value);
+          }
+        } else {
+          init = init.add(
+            new Decimal(posting.as.amount.value)
+              .mul(new Decimal(posting.amount.value))
+              .toNumber()
+          );
+        }
+      } else if (posting.held) {
+        if (posting.held.type === "cost") {
+          if (posting.amount.value < 0) {
+            init = init.minus(posting.held.amount.value);
+          } else {
+            init = init.add(posting.held.amount.value);
+          }
+        } else {
+          init = init.add(
+            new Decimal(posting.held.amount.value)
+              .mul(new Decimal(posting.amount.value))
+              .toNumber()
+          );
+        }
+      } else {
+        init = init.add(posting.amount.value);
+      }
     });
     const newPosting = [
       ...old.postings,
